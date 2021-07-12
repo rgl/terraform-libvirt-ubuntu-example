e16070ec4bf0bbdbc5c6cf17e33d423d16a33899
Rui Lopes <rgl@ruilopes.com>
2021-07-12 20:12:23 +0100

    upgrade to libvirt provider 0.6.10

--- main.tf
+++ main.tf
@@ -12,10 +12,11 @@ terraform {
       source = "hashicorp/template"
       version = "2.2.0"
     }
+    # see https://registry.terraform.io/providers/dmacvicar/libvirt
     # see https://github.com/dmacvicar/terraform-provider-libvirt
     libvirt = {
       source = "dmacvicar/libvirt"
-      version = "0.6.3"
+      version = "0.6.10"
     }
   }
 }
