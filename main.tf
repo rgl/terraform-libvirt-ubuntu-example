e22e56074955c04933830e55eb5b614511fc48ec
Rui Lopes <rgl@ruilopes.com>
2022-09-09 12:44:41 +0100

    upgrade to dmacvicar/libvirt 0.6.14

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ terraform {
     # see https://github.com/dmacvicar/terraform-provider-libvirt
     libvirt = {
       source = "dmacvicar/libvirt"
-      version = "0.6.10"
+      version = "0.6.14"
     }
   }
 }
@@ -29,7 +29,7 @@ variable "prefix" {
   default = "terraform_example"
 }
 
-# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.6.3/website/docs/r/network.markdown
+# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.6.14/website/docs/r/network.markdown
 resource "libvirt_network" "example" {
   name = var.prefix
   mode = "nat"
@@ -46,12 +46,12 @@ resource "libvirt_network" "example" {
 
 # create a cloud-init cloud-config.
 # NB this creates an iso image that will be used by the NoCloud cloud-init datasource.
-# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.6.3/website/docs/r/cloudinit.html.markdown
+# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.6.14/website/docs/r/cloudinit.html.markdown
 # see journactl -u cloud-init
 # see /run/cloud-init/*.log
 # see https://cloudinit.readthedocs.io/en/latest/topics/examples.html#disk-setup
 # see https://cloudinit.readthedocs.io/en/latest/topics/datasources/nocloud.html#datasource-nocloud
-# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.6.3/libvirt/cloudinit_def.go#L133-L162
+# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.6.14/libvirt/cloudinit_def.go#L133-L162
 resource "libvirt_cloudinit_disk" "example_cloudinit" {
   name = "${var.prefix}_example_cloudinit.iso"
   user_data = <<EOF
@@ -83,7 +83,7 @@ EOF
 }
 
 # this uses the vagrant ubuntu image imported from https://github.com/rgl/ubuntu-vagrant.
-# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.6.3/website/docs/r/volume.html.markdown
+# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.6.14/website/docs/r/volume.html.markdown
 resource "libvirt_volume" "example_root" {
   name = "${var.prefix}_root.img"
   base_volume_name = "ubuntu-20.04-amd64_vagrant_box_image_0_box.img"
@@ -92,17 +92,17 @@ resource "libvirt_volume" "example_root" {
 }
 
 # a data disk.
-# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.6.3/website/docs/r/volume.html.markdown
+# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.6.14/website/docs/r/volume.html.markdown
 resource "libvirt_volume" "example_data" {
   name = "${var.prefix}_data.img"
   format = "qcow2"
   size = 6*1024*1024*1024 # 6GiB.
 }
 
-# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.6.3/website/docs/r/domain.html.markdown
+# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.6.14/website/docs/r/domain.html.markdown
 resource "libvirt_domain" "example" {
   name = var.prefix
-  cpu = {
+  cpu {
     mode = "host-passthrough"
   }
   vcpu = 2
